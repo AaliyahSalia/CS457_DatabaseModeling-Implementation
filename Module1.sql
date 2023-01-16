b8cf84bdfa4750c45c6d537da0ec972faf88a702
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2023-01-16 14:25:04 -0800

    Create Module1.sql

---
+++ Module1.sql
@@ -0,0 +1,50 @@
+The Friendly Car Dealership Attributes for ER Diagram 
+
+Cars: carID, type/Model, color, seriesNumber, year, fuelType, price, city, state, currentMileage, annualFuelCost, carCategory, carCondition, complimentaryProgram, finalPrice, greenhouseGasRating, horsePower, modelName, seats, brand
+
+Customer: custID, name(first,last), Address(street,city,state,zip), PhoneNumber(areacode, telephone number), email, Gender, annual_Income, birthdate
+
+Employee: EmpID, name(first,last), Address(street,city,state,zip), PhoneNumber(areacode, telephone number), email, Gender, salary, dateOfJoining, department, SSN
+
+SalesPerson: EmpID(fk), name(first,last), commissioneRate, numberofCarSold, targetAmount, targetAchieved, salesYear, year_experiance,SSN
+
+SalesInvoice: invoiceID, custID(fk), EmpID(fk), insuranceID(fk), finalPrice, invoiceDate
+
+Showroom: name, address (street,city,state,zip), branches, no_of_employee, carCategory,
+
+Insurance: insuranceID, carID(fk), policyNumber, insuranceCompany, insuranceValidity, registrationDate, warrantyInformation, city, State, vehicleRegistrationFee
+
+Tax: taxID, totalTax, customerTaxID
+
+Survey: surveyID, surveyDate, custID(fk), rateCar, rateDelarship, rateSalesperson
+
+
+
+
+The Friendly Car Dealership Attributes for EER Diagram 
+
+
+Person: Attributes: pid, name(firstName, lastName),address(street, city, state, zip), phone(areaCode,telephoneNumber)
+
+
+Cars :additional attributes: financing, functionality, optionalEquipment, licensePlateFee,licenseNumber,numberOfCarSold,overallVehicleScore,standardFeatures,Interior, Exterior, Functional, Safety, Security,basicInformation
+
+Customer:subset of person,additional attributes:  Occupation,status,drivingLicence,idProof,preferredContactDetail,preferedCarBrand,preferedCarModel,preferedCarColor,preferedCarType
+
+Employee:subset of person, additional attributes: employee_location,interviewDate,interviewName,idProof
+
+SalesPerson: subset of person, additional attributes: lastYearSalesCost,lastYearCommissionEarned,languageKnown
+
+CarOwner: subset of customer, attributes: ownerId,carSoldDate,finalPrice,SSN
+
+Showroom:additional attributes:numberOfcarStock,carModel
+
+Finance:subset of showroom, totalSalesAmount, revenue, numberOfCarSold, lastYearRevenue, lastYearNumberOfCarSold, totalEmpSalary, percentageProfit/Loss,miscellaneous
+
+Tax:additional attributes:
+
+Survey:additional attributes:
+
+
+
+
